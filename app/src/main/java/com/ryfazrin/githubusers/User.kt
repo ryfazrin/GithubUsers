451c6c4969910f88d41843d3c8cfddf8deadd977
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-30 13:17:57 +0800

    Update User.kt
    menmbahkan data baru ke data class User

--- app/src/main/java/com/ryfazrin/githubusers/User.kt
+++ app/src/main/java/com/ryfazrin/githubusers/User.kt
@@ -8,9 +8,9 @@ data class User(
     var name: String,
     var username: String,
     var location: String,
+    var avatar: Int,
     var repository: String,
     var company: String,
     var followers: String,
-    var following: String,
-    var avatar: Int
+    var following: String
 ) : Parcelable
