e3ff929d6c2744a5aea43908e295c83b86e74e97
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-30 11:32:50 +0800

    Update MainActivity.kt
    Menambahkan Adapter ke RecyclerView

--- app/src/main/java/com/ryfazrin/githubusers/MainActivity.kt
+++ app/src/main/java/com/ryfazrin/githubusers/MainActivity.kt
@@ -2,10 +2,42 @@ package com.ryfazrin.githubusers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvUser: RecyclerView
+    private val list = ArrayList<User>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvUser = findViewById(R.id.rv_user)
+        rvUser.setHasFixedSize(true)
+
+        list.addAll(listUser)
+        showRecyclerList()
+    }
+
+    private val listUser: ArrayList<User>
+        get() {
+            val dataName = resources.getStringArray(R.array.name)
+            val dataUsername = resources.getStringArray(R.array.username)
+            val dataLocation = resources.getStringArray(R.array.location)
+            val dataAvatar = resources.obtainTypedArray(R.array.avatar)
+            val listUser = ArrayList<User>()
+            for (i in dataName.indices) {
+                val user = User(dataName[i], dataUsername[i], dataLocation[i], dataAvatar.getResourceId(i, -1))
+                listUser.add(user)
+            }
+            return listUser
+        }
+
+    private fun showRecyclerList(){
+        rvUser.layoutManager = LinearLayoutManager(this)
+        val listUserAdapter = ListUserAdapter(list)
+        rvUser.adapter = listUserAdapter
     }
 }
