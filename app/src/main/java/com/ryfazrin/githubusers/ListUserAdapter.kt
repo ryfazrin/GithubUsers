108931bc58f32c8aac621186bd88a3b8cab554a8
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-30 10:44:56 +0800

    Create ListUserAdapter.kt
    Create class ListViewHolder

---
+++ app/src/main/java/com/ryfazrin/githubusers/ListUserAdapter.kt
@@ -0,0 +1,9 @@
+package com.ryfazrin.githubusers
+
+import androidx.recyclerview.widget.RecyclerView
+
+class ListUserAdapter : RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
+    class ListViewHolder {
+
+    }
+}
