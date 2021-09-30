f57943b0a39a411fb6f2f532efa8817efb335918
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-30 10:45:37 +0800

    Update ListUserAdapter.kt
    Implement Members ListUserAdapter.
    onCreateViewHolder, onBindViewHolder, getItemCount

--- app/src/main/java/com/ryfazrin/githubusers/ListUserAdapter.kt
+++ app/src/main/java/com/ryfazrin/githubusers/ListUserAdapter.kt
@@ -1,9 +1,22 @@
 package com.ryfazrin.githubusers
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class ListUserAdapter : RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
     class ListViewHolder {
 
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 }
