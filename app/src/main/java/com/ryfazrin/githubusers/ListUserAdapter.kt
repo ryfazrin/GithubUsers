bc7fbacbdc2bded08dd6f22e2b67f17053aa05c5
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-30 11:11:11 +0800

    Update ListUserAdapter.kt

--- app/src/main/java/com/ryfazrin/githubusers/ListUserAdapter.kt
+++ app/src/main/java/com/ryfazrin/githubusers/ListUserAdapter.kt
@@ -1,23 +1,34 @@
 package com.ryfazrin.githubusers
 
+import android.media.Image
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ListUserAdapter(private val listUser: ArrayList<User>) : RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
     class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        var imagePhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+        var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        var tvUsername: TextView = itemView.findViewById(R.id.tv_item_username)
+        var tvLocation: TextView = itemView.findViewById(R.id.tv_item_location)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_row_user, viewGroup, false)
+        return ListViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val (name, username, location, repository, company, followers, following, avatar) = listUser[position]
+        holder.imagePhoto.setImageResource(avatar)
+        holder.tvName.text = name
+        holder.tvUsername.text = username
+        holder.tvLocation.text = location
+
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = listUser.size
 }
