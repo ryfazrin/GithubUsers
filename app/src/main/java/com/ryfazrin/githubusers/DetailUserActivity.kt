28a481a61b03755680795ca09189c46f92bb82c7
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-30 14:59:19 +0800

    Update DetailUserActivity.kt
    Menambahkan data class User

--- app/src/main/java/com/ryfazrin/githubusers/DetailUserActivity.kt
+++ app/src/main/java/com/ryfazrin/githubusers/DetailUserActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 
 class DetailUserActivity : AppCompatActivity() {
 
@@ -19,16 +20,24 @@ class DetailUserActivity : AppCompatActivity() {
         val tvName: TextView = findViewById(R.id.tv_detail_name)
         val tvFollowers: TextView = findViewById(R.id.tv_detail_followers)
         val tvFollowing: TextView = findViewById(R.id.tv_detail_following)
+        val tvLocation: TextView = findViewById(R.id.tv_detail_location)
+        val tvCompany: TextView = findViewById(R.id.tv_detail_company)
+        val tvRepository: TextView = findViewById(R.id.tv_detail_repository)
 
         // data User
         val user = intent.getParcelableExtra<User>(EXTRA_USER) as User
 
         supportActionBar?.title = user.username
 
-//        val text = "Name : ${user.name},\nRepository : ${user.repository}"
-        imgUser.setImageResource(user.avatar)
+
+        Glide.with(this)
+            .load(user.avatar)
+            .into(imgUser)
         tvName.text = user.name
         tvFollowers.text = user.followers
         tvFollowing.text = user.following
+        tvLocation.text = user.location
+        tvCompany.text = user.company
+        tvRepository.text = user.repository
     }
 }
