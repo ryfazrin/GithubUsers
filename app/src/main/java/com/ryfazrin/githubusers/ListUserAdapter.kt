07a6c235968044e4cb6e20018b7c96e5d1c332a0
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-30 10:52:07 +0800

    Update ListUserAdapter.kt
    membuat construct list class

--- app/src/main/java/com/ryfazrin/githubusers/ListUserAdapter.kt
+++ app/src/main/java/com/ryfazrin/githubusers/ListUserAdapter.kt
@@ -4,7 +4,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class ListUserAdapter : RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
+class ListUserAdapter(private val listUser: ArrayList<User>) : RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
     class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     }
