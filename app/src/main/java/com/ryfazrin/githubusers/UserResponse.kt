e5b5305fd4fc073d3da045065f5e4c26f6595290
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-16 10:32:24 +0800

    Create UserResponse.kt

---
+++ app/src/main/java/com/ryfazrin/githubusers/UserResponse.kt
@@ -0,0 +1,37 @@
+package com.ryfazrin.githubusers
+
+data class UserResponse(
+	val gistsUrl: String,
+	val reposUrl: String,
+	val followingUrl: String,
+	val twitterUsername: Any,
+	val bio: String,
+	val createdAt: String,
+	val login: String,
+	val type: String,
+	val blog: String,
+	val subscriptionsUrl: String,
+	val updatedAt: String,
+	val siteAdmin: Boolean,
+	val company: Any,
+	val id: Int,
+	val publicRepos: Int,
+	val gravatarId: String,
+	val email: Any,
+	val organizationsUrl: String,
+	val hireable: Any,
+	val starredUrl: String,
+	val followersUrl: String,
+	val publicGists: Int,
+	val url: String,
+	val receivedEventsUrl: String,
+	val followers: Int,
+	val avatarUrl: String,
+	val eventsUrl: String,
+	val htmlUrl: String,
+	val following: Int,
+	val name: String,
+	val location: Any,
+	val nodeId: String
+)
+
