056cb6c3bd0854bd693956c845937ff2ae4987a3
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-16 16:23:06 +0800

    Update ApiService.kt
    Buat API get user detail

--- app/src/main/java/com/ryfazrin/githubusers/ApiService.kt
+++ app/src/main/java/com/ryfazrin/githubusers/ApiService.kt
@@ -3,9 +3,16 @@ package com.ryfazrin.githubusers
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 
 interface ApiService {
     @Headers("Authorization: token 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")
+
     @GET("users")
     fun getUser(): Call<List<Users>>
+
+    @GET("users/{login}")
+    fun getDetailUser(
+        @Path("login") login: String
+    ): Call<UserDetailResponse>
 }
