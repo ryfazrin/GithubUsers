fc19573de2258b834b4ea0bc7428561a78eef8b1
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-18 13:17:08 +0800

    Update ApiService.kt
    Membuat API data search users.
    Source :
    https://stackoverflow.com/questions/65954502/java-lang-illegalargumentexception-url-query-string-for-dynamic-query-parameter

--- app/src/main/java/com/ryfazrin/githubusers/API/ApiService.kt
+++ app/src/main/java/com/ryfazrin/githubusers/API/ApiService.kt
@@ -2,15 +2,23 @@ package com.ryfazrin.githubusers.API
 
 import com.ryfazrin.githubusers.UserDetailResponse
 import com.ryfazrin.githubusers.Users
+import com.ryfazrin.githubusers.UsersSearch
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
     @Headers("Authorization: token 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")
     @GET("users")
     fun getUser(): Call<List<Users>>
+
+    @Headers("Authorization: token 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")
+    @GET("search/users?")
+    fun getSearchUser(
+        @Query("q") q: String
+    ): Call<UsersSearch>
 
     @Headers("Authorization: token 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")
     @GET("users/{login}")
