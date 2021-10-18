dd8c5aa7d059d52ea90428965412175c32ab0ccf
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-18 16:32:17 +0800

    Update FollowersFragment.kt

--- app/src/main/java/com/ryfazrin/githubusers/FollowersFragment.kt
+++ app/src/main/java/com/ryfazrin/githubusers/FollowersFragment.kt
@@ -1,6 +1,5 @@
 package com.ryfazrin.githubusers
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -13,15 +12,10 @@ import com.ryfazrin.githubusers.databinding.FragmentFollowersBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.util.zip.Inflater
 
 class FollowersFragment : Fragment() {
 
     private lateinit var binding: FragmentFollowersBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,7 +37,7 @@ class FollowersFragment : Fragment() {
         val data = requireArguments().getString(EXTRA_USER).toString()
 
         // test ambil data
-        Log.d("fragmentfollow", "Tes: ${data}")
+//        Log.d("fragmentfollow", "Tes: ${data}")
 //        binding.testText.text = data
         // val client = ApiConfig.getApiService().getDetailFollowers(DetailUserActivity.EXTRA_USER)
         val client = ApiConfig.getApiService().getDetailFollowers(data)
@@ -71,7 +65,7 @@ class FollowersFragment : Fragment() {
 
         binding.rvFollowers.apply {
             layoutManager = LinearLayoutManager(requireContext()) // activity or requireContext()
-            adapter = FollowerAdapter(listFollower)
+            adapter = FollowAdapter(listFollower)
 //            adapter = ListUserAdapter(listFollower)
         }
 //        val adapter = FollowerAdapter(listFollower)
