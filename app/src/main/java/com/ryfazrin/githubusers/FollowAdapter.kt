554273e3df241f815497bfab3d140d29be4c2ac4
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-18 14:51:07 +0800

    change name Adapter and item_row
    Mengubah nama FollowerAdapter dan item_row_follower jadi FollowAdapter dan item_rom_follow

---
+++ app/src/main/java/com/ryfazrin/githubusers/FollowAdapter.kt
@@ -0,0 +1,37 @@
+package com.ryfazrin.githubusers
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class FollowAdapter(private val listFollowers: ArrayList<Users>) : RecyclerView.Adapter<FollowAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imgFollowPhoto: ImageView = itemView.findViewById(R.id.img_follow_photo)
+        val tvFollowUsername: TextView = itemView.findViewById(R.id.tv_follow_username)
+        val tvFollowType: TextView = itemView.findViewById(R.id.tv_follow_type)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_row_follow, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: FollowAdapter.ViewHolder, position: Int) {
+        val (login, avatar_url, type) = listFollowers[position]
+        Glide.with(holder.itemView.context)
+            .load(avatar_url)
+            .circleCrop()
+            .into(holder.imgFollowPhoto)
+        holder.tvFollowUsername.text = login
+        holder.tvFollowType.text = type
+    }
+
+    override fun getItemCount(): Int = listFollowers.size
+}
