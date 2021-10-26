9157c5669dda5049152953a392bf1972058fd25c
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-26 14:19:27 +0800

    Update SettingsActivity.kt
    Menambahkan fungsi mengaktifkan dark mode. Namun, apabila aplikasi close data dark mode belum bisa tersimpan.

--- app/src/main/java/com/ryfazrin/githubusers/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/ryfazrin/githubusers/ui/settings/SettingsActivity.kt
@@ -2,10 +2,12 @@ package com.ryfazrin.githubusers.ui.settings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
 import com.ryfazrin.githubusers.R
 import com.ryfazrin.githubusers.databinding.ActivitySettingsBinding
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener {
 
     private lateinit var binding: ActivitySettingsBinding
 
@@ -17,10 +19,22 @@ class SettingsActivity : AppCompatActivity() {
 
         supportActionBar?.title = getString(R.string.settings)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.switchTheme.setOnCheckedChangeListener(this)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    override fun onCheckedChanged(compoundButton: CompoundButton?, isChecked: Boolean) {
+        if (isChecked) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            binding.switchTheme.isChecked = true
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            binding.switchTheme.isChecked = false
+        }
     }
 }
