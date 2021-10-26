3b2d2e9879b484231e3c82d37a5bc720e0e3954a
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-26 10:17:02 +0800

    Update UserResponse.kt
    hapus Variable yang tidak pernah digunakan

--- app/src/main/java/com/ryfazrin/githubusers/data/UserResponse.kt
+++ app/src/main/java/com/ryfazrin/githubusers/data/UserResponse.kt
@@ -45,12 +45,6 @@ data class Users(
 
 @Parcelize
 data class UsersSearch(
-	@field:SerializedName("total_count")
-	val totalCount: Int,
-
-	@field:SerializedName("incomplete_results")
-	val incompleteResults: Boolean,
-
 	@field:SerializedName("items")
 	val items: List<Users>
 ) : Parcelable
