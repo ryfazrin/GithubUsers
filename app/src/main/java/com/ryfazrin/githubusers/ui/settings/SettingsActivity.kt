be520e8f4a6351a2f0ac1817cda196f7ce463394
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-26 14:49:47 +0800

    Update SettingsActivity.kt

--- app/src/main/java/com/ryfazrin/githubusers/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/ryfazrin/githubusers/ui/settings/SettingsActivity.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatDelegate
 import com.ryfazrin.githubusers.R
 import com.ryfazrin.githubusers.databinding.ActivitySettingsBinding
 
-class SettingsActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener {
+class SettingsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySettingsBinding
 
@@ -20,21 +20,19 @@ class SettingsActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeList
         supportActionBar?.title = getString(R.string.settings)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        binding.switchTheme.setOnCheckedChangeListener(this)
+        binding.switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                binding.switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                binding.switchTheme.isChecked = false
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
-    }
-
-    override fun onCheckedChanged(compoundButton: CompoundButton?, isChecked: Boolean) {
-        if (isChecked) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            binding.switchTheme.isChecked = true
-        } else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            binding.switchTheme.isChecked = false
-        }
     }
 }
