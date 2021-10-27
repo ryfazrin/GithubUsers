c54cb23bed220fec3cc86bfc21d5601b20c9488d
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-27 13:02:21 +0800

    Clean code

--- app/src/main/java/com/ryfazrin/githubusers/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/ryfazrin/githubusers/ui/settings/SettingsActivity.kt
@@ -1,6 +1,5 @@
 package com.ryfazrin.githubusers.ui.settings
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CompoundButton
